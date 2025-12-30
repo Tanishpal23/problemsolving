48f5bb4e5ac1de69ad60ed66cdeeb41f9974f495
Tanishpal23 <124513642+Tanishpal23@users.noreply.github.com>
2025-12-30 23:15:14 +0530

    Time: 8 ms (54.01%) | Memory: 26 MB (84.29%) - LeetSync

--- 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.cpp
@@ -1,30 +1,32 @@
 // Approach
-// Brute Force
+// 1.for every word make a no using a,b,c.... as bit 0,1,2,
+// 2.then take bitwise and operation of each term
+// 3.if bitwise and == 0, no matching (bit digit) -> no matching char
 
 class Solution {
 public:
     int maxProduct(vector<string>& words) {
         int n = words.size();
 
+        vector<int> bitMask(n, 0);
+
+        for(int i=0; i<n; i++){
+
+            for(char c: words[i]){
+                bitMask[i] |= (1<<(c-'a'));
+            }
+        }
+
         int ans = 0;
 
         for(int i=0; i<n-1; i++){
-            unordered_map<char, int> mpp1;
-            for(auto c: words[i]) mpp1[c] = 1;
-
             for(int j=i+1; j<n; j++){
-                bool isPre = false;
-
-                for(char c: words[j]){
-                    if(mpp1.count(c)){
-                        isPre = true;
-                        break;
-                    }
+                if((bitMask[i] & bitMask[j]) == 0){
+                    ans = max(ans, (int)words[i].size() * (int)words[j].size());
                 }
-
-                if(!isPre) ans = max(ans, (int)words[i].size()*(int)words[j].size());
             }
         }
+
         return ans;
     }
 };
