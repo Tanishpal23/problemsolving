b29ae3f9db3502ff2ec7d9bd2e94e3855bee979b
Tanishpal23 <124513642+Tanishpal23@users.noreply.github.com>
2026-01-03 16:27:30 +0530

    Time: 59 ms (28.75%) | Memory: 76.3 MB (22.09%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.cpp
+++ 922-possible-bipartition/possible-bipartition.cpp
@@ -1,64 +1,63 @@
 // Approach
-// Bipartitiate graph
+// Bipartite graph
 // dislikes -> should have different colours.
 // Make graph of dislikes -> assign different colours (group) to adjacent nodes
 
-// TC O(n + m) -> n nodes , m -> edges
-// SC O(n + m)
+// TC: O(n + m)   (n = nodes, m = dislikes/edges)
+// SC: O(n + m)
 
 class Solution {
 public:
+
+    bool dfs(int node, int currGrp, vector<vector<int>>& adjList, vector<int>& grp) {
+
+        // assign current group
+        grp[node] = currGrp;
+
+        // visit all neighbours
+        for(auto neighbour : adjList[node]) {
+
+            // if neighbour already assigned
+            if(grp[neighbour] != -1) {
+                // conflict: same group on both ends
+                if(grp[neighbour] == currGrp)
+                    return false;
+            }
+            // if neighbour not assigned, assign opposite group
+            else {
+                if(!dfs(neighbour, !currGrp, adjList, grp))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
 
-        //Graph inititalisation in form of adjList 
+        // Graph initialisation in form of adjList
         vector<vector<int>> adjList(n+1);
-        for(auto edge: dislikes){
-            int u = edge[0], v=edge[1];
+        for(auto edge : dislikes) {
+            int u = edge[0], v = edge[1];
             adjList[u].push_back(v);
             adjList[v].push_back(u);
         }
 
-        //group will be assigned here
+        // group assignment array
         vector<int> grp(n+1, -1);
 
-        //queue for bfs traversal
-        queue<int> q;
+        // check for all connected components
+        for(int i = 1; i <= n; i++) {
 
-        //check for connected components of graph
-        for(int i=1; i<=n; i++){
+            // if node not visited yet
+            if(grp[i] == -1) {
 
-            //if not grouped yet
-            if(grp[i]==-1){
-                
-                //assign 0th group initially
-                q.push(i);
-                grp[i]=0;
-
-                //bfs starts
-                while(!q.empty()){
-
-                    int node = q.front();
-                    q.pop();
-
-                    //check and assign neighbours of curr node
-                    for(auto neighbour: adjList[node]){
-                        
-                        //if neighbour already assigned grp check if grp same as node 
-                        if(grp[neighbour] != -1 && grp[neighbour]==grp[node]) return false;
-                        
-                        //if neighbour not visited yet assign different grp than curr node grp
-                        if(grp[neighbour]==-1){
-                            grp[neighbour] = !grp[node];
-                            q.push(neighbour);
-                        }
-
-                    }
-                }
+                // start DFS with group 0
+                if(!dfs(i, 0, adjList, grp))
+                    return false;
             }
         }
-        
-        //if all nodes provided grp return true
-        return true;
 
+        return true;
     }
 };
