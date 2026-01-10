84eb687d794bef02cea98221508befaa65eeea54
Tanishpal23 <124513642+Tanishpal23@users.noreply.github.com>
2026-01-10 21:02:23 +0530

    Time: 15 ms (74.15%) | Memory: 18.3 MB (49.34%) - LeetSync

---
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -0,0 +1,36 @@
+// Approach
+// dp[i][j] = minimum ascii delete sum to make s1[0....i-1] and s2[0....j-1]
+
+class Solution {
+public:
+    int minimumDeleteSum(string s1, string s2) {
+        int n = s1.size(), m = s2.size();
+
+        // dp[i][j] = minimum ASCII delete sum to make
+        // s1[0..i-1] and s2[0..j-1] equal
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        // base cases
+        for (int i = 1; i <= n; i++)
+            dp[i][0] = dp[i - 1][0] + s1[i - 1];
+
+        for (int j = 1; j <= m; j++)
+            dp[0][j] = dp[0][j - 1] + s2[j - 1];
+
+        // fill dp
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min(
+                        dp[i - 1][j] + s1[i - 1],  // delete from s1
+                        dp[i][j - 1] + s2[j - 1]   // delete from s2
+                    );
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+};
