a5ba383ec6ee1814f12b46e4e7aacf9aab203be3
Tanishpal23 <124513642+Tanishpal23@users.noreply.github.com>
2026-01-14 17:34:55 +0530

    Time: 27 ms (87.95%) | Memory: 122 MB (45.32%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -8,23 +8,20 @@ public:
 
         vector<int> oneRow(n, 0);
         vector<int> oneCol(m, 0);
-        vector<int> zeroRow(n, 0);
-        vector<int> zeroCol(m, 0);
 
         for(int i=0; i<n; i++){
 
             for(int j=0; j<m; j++){
+                
                 int row = i;
                 int col = j;
 
                 if( grid[i][j] == 1 ){
                     oneRow[row]++;
                     oneCol[col]++;
-                }else{
-                    zeroRow[row]++;
-                    zeroCol[col]++;
                 }
             }
+
         }
 
         vector<vector<int>> ans(n, vector<int>(m, 0));
@@ -32,7 +29,7 @@ public:
             for(int j=0; j<m; j++){
                 int row = i;
                 int col = j;
-                ans[i][j] = oneRow[row] + oneCol[col] - zeroRow[row] - zeroCol[col];
+                ans[i][j] = oneRow[row] + oneCol[col] - (n-oneRow[row]) - (m-oneCol[col]);
             }
         }
 
