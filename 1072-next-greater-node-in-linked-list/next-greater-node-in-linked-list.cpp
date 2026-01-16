319ecb492792823f1f5147938ba662e22309066d
Tanishpal23 <124513642+Tanishpal23@users.noreply.github.com>
2026-01-16 19:00:50 +0530

    Time: 610 ms (13.74%) | Memory: 42.3 MB (97.86%) - LeetSync

---
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+
+    int n = 0;
+
+    ListNode* reverselist(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* temp = NULL;
+
+        while( curr ){
+            temp = curr->next;
+            curr -> next = prev;
+            prev = curr;
+            curr = temp;
+            n++;
+
+            cout << n << "  " << endl;
+        }
+
+        return prev;
+    }
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        ListNode* newHead = reverselist(head);
+        ListNode* curr = newHead;
+
+        vector<int> ans(n, 0);
+        stack<int> st;
+
+        st.push(curr->val);
+        curr = curr->next;
+
+        for(int i=1; i<n; i++){
+
+            int currVal = curr->val;
+            int topVal = st.empty()? 0:  st.top();
+
+            while( !st.empty() && currVal >= st.top() ) st.pop();
+
+            if( !st.empty() ) ans[n-i-1] = st.top();
+            
+            st.push( currVal );
+            curr = curr->next;
+        }
+
+        return ans;
+    }
+};
