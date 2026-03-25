48ab622676e26d995fb30f4fda69d32fcbdac601
Tanishpal23 <124513642+Tanishpal23@users.noreply.github.com>
2026-03-25 23:47:38 +0530

    Time: 15 ms (52.96%) | Memory: 9.2 MB (92.20%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-
-    int n;
-    int start = 0;
-    int maxLen = 0;
-
-    void findInd(string s, int l, int r){
-
-        while( l>=0 && r<n && s[l]==s[r] ){
-            l--;
-            r++;
-        }
-
-        int len = r-l-1;
-        if( len > maxLen ){
-            start = l+1;
-            maxLen = len;
-        }
-
-        return;
-    }
     string longestPalindrome(string s) {
         
-        n = s.size();
+        int n = s.size();
 
-        for( int i=0; i<n; i++ ){
-            findInd( s, i, i );
-            findInd( s, i, i+1 );
+        if(!n) return "";
+
+        int start = 0;
+        int maxlen = 1;
+
+
+        auto expand = [&](int l, int r){
+            while(l >= 0 && r < n && s[l] == s[r]){
+                if(r-l+1 > maxlen){
+                    maxlen = r-l+1;
+                    start = l;
+                }
+
+                l--;
+                r++;
+            }
+        };
+
+
+        for(int i=0; i<n; i++){
+            expand(i, i);
+            expand(i, i+1);
         }
 
-        return s.substr( start, maxLen );
-
+        return s.substr(start, maxlen);
     }
 };
